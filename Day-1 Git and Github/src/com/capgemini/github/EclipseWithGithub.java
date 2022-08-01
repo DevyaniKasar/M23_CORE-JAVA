8c644aeaee65ec26d514de34b4fbd992b88059e6
hp <hp@DESKTOP-COOUTF6>
2022-08-01 15:49:55 +0530

    First Commit

---
+++ Day-1 Git and Github/src/com/capgemini/github/EclipseWithGithub.java
@@ -0,0 +1,10 @@
+package com.capgemini.github;
+
+public class EclipseWithGithub {
+
+	public static void main(String[] args) {
+		//print the line
+   System.out.println("Welcome to Capgemini C2TC Program");
+	}
+
+}
