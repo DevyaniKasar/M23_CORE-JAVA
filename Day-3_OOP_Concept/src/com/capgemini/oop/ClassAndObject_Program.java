e5188d6965865226aeec6ccb266e55057e265463
hp <hp@DESKTOP-COOUTF6>
2022-08-03 23:18:02 +0530

    first commit

---
+++ Day-3_OOP_Concept/src/com/capgemini/oop/ClassAndObject_Program.java
@@ -0,0 +1,26 @@
+package com.capgemini.oop;
+//program on class and object
+
+
+class A {
+	//variable
+	   int  a;
+	   //user-defined method
+	   void display() {
+		   System.out.println("welcome");
+	   }
+}
+public class ClassAndObject_Program {
+       
+       
+	public static void main(String[] args) {
+		//object creation
+		A obj=new A();
+		//accessing the class method
+	    obj.display();
+	    //accessing the class variable
+	    obj.a=5;
+		System.out.println(obj.a);
+	}
+}
+
