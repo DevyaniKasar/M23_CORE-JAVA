703da1b0d055b5d6cf6738502edb1b28f16bf479
hp <hp@DESKTOP-COOUTF6>
2022-08-22 15:26:38 +0530

    Second

--- Day-14_Set,QueueAndMAp/src/com/cg/Set/HashSetExample.java
+++ Day-14_Set,QueueAndMAp/src/com/cg/Set/HashSetExample.java
@@ -3,12 +3,12 @@ package com.cg.Set;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SetExample {
+public class HashSetExample {
 
 	public static void main(String[] args) {
 		//Set<Integer>obj=new HashSet<Integer>();
 		Set<Object>obj=new HashSet<Object>();
-		//1.Set is an unordered
+		//1.HashSet is an unordered
 		obj.add("Prerna");
 		obj.add('p');
 		obj.add(5.8);
