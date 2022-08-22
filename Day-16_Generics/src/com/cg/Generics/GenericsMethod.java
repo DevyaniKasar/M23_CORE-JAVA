7a1f3c53c5c6f9a7d8ff7938a3abeddd6df95b22
hp <hp@DESKTOP-COOUTF6>
2022-08-22 15:22:01 +0530

    First

---
+++ Day-16_Generics/src/com/cg/Generics/GenericsMethod.java
@@ -0,0 +1,32 @@
+package com.cg.Generics;
+
+    public class GenericsMethod 
+    {
+	//Generic Method: We can pass any type of Arguments
+	//E is a Element
+			public static <E>void printArray(E[] elem)
+			{
+				for(E itr:elem)
+				{
+					//to check class name
+					System.out.println(itr.getClass().getName());
+					System.out.println(itr);
+				}
+				System.out.println();
+				
+			}
+
+	public static void main(String[] args) 
+	{
+		
+		Integer arr[]= {11,22,33};
+		Character ch[]= {'P','Q','R'};
+		
+		System.out.println("Integer Array: ");
+		printArray(arr);
+		
+		System.out.println("Character Array: ");
+		printArray(ch);
+	}
+	
+}
